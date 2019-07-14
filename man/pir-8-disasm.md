e560410a71fe0f8355832067c4c9f867b88360d2
nabijaczleweli <nabijaczleweli@gmail.com>
2019-07-14 13:44:03 +0200

    Add pir-8-disasm option parsing and doc

---
+++ man/pir-8-disasm.md
@@ -0,0 +1,105 @@
+pir-8-disasm(1) -- Disassembler for the pir-8
+=============================================
+
+## SYNOPSIS
+
+`pir-8-disasm` [OPTIONS] <FILE>
+
+## DESCRIPTION
+
+Disassembler for the pir-8.
+
+Specified input file (or "-" for stdin) is disassembled into stdout.
+
+## OPTIONS
+
+  -e BYTES
+
+    Skip BYTES bytes of header
+
+  -k START,BYTES...
+
+    Don't disassemble BYTES bytes from position START
+
+    Can be specified multiple times
+
+## Exit values
+
+    1 - option parsing error
+    2 - output file creation failure
+    3 - input file opening failure
+    4 - output write failure
+    5 - input read failure
+    6 - instruction parse error
+    7 - instruction data parse error
+
+## EXAMPLES
+
+  `pir-8-disasm test-data/xor-swap-with-loads.p8b`
+
+    00000000   24   LOAD IND A
+    00000002 0110 D 0x0110
+    00000003   1D   LOAD IMM B
+    00000004   69 D 0x69
+    00000005   62   MOVE A X
+    00000006   6B   MOVE B Y
+    00000007   35   ALU XOR
+    00000008   4C   MOVE S A
+    00000009   63   MOVE A Y
+    0000000A   6A   MOVE B X
+    0000000B   35   ALU XOR
+    0000000C   4D   MOVE S B
+    0000000D   62   MOVE A X
+    0000000E   6B   MOVE B Y
+    0000000F   35   ALU XOR
+    00000010   4C   MOVE S A
+    00000011   FF   HALT
+
+  `pir-8-disasm -e 3 test-data/xor-swap-with-loads.p8b`
+
+    00000000   1D   LOAD IMM B
+    00000001   69 D 0x69
+    00000002   62   MOVE A X
+    00000003   6B   MOVE B Y
+    00000004   35   ALU XOR
+    00000005   4C   MOVE S A
+    00000006   63   MOVE A Y
+    00000007   6A   MOVE B X
+    00000008   35   ALU XOR
+    00000009   4D   MOVE S B
+    0000000A   62   MOVE A X
+    0000000B   6B   MOVE B Y
+    0000000C   35   ALU XOR
+    0000000D   4C   MOVE S A
+    0000000E   FF   HALT
+
+  `pir-8-disasm -k 1,7 test-data/xor-swap-with-loads.p8b`
+
+    00000000   24   LOAD IND A
+    00000002 0110 D 0x0110
+    00000003   1D   LOAD IMM B
+    00000004   69 D 0x69
+    00000005   62   MOVE A X
+    00000006   6B   MOVE B Y
+    00000007   35   ALU XOR
+    00000008        skipping 0x07 bytes
+    00000010   4C   MOVE S A
+    00000011   FF   HALT
+
+## AUTHOR
+
+Written by nabijaczleweli &lt;<nabijaczleweli@gmail.com>&gt;
+
+## SPECIAL THANKS
+
+To all who support further development, in particular:
+
+  * ThePhD
+
+## REPORTING BUGS
+
+&lt;<https://github.com/LoungeCPP/pir-8-emu/issues>&gt;
+
+## SEE ALSO
+
+&lt;<https://github.com/LoungeCPP/pir-8-emu>&gt;
--- src/options.rs
+++ src/options.rs
@@ -12,12 +12,15 @@
 //! ```
 
 
+use self::super::util::parse_with_prefix;
 use clap::{AppSettings, App, Arg};
 use std::path::PathBuf;
 use std::fs;
 
 
 /// Representation of the assembler's all configurable values.
+///
+/// Interface based on GNU AS.
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct AssemblerOptions {
     /// The input assembly files, with `None` being stdin
@@ -34,6 +37,25 @@ pub struct AssemblerOptions {
     pub output: Option<(String, PathBuf)>,
 }
 
+/// Representation of the assembler's all configurable values.
+///
+/// Interface based on `ndisasm`.
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub struct DisassemblerOptions {
+    /// The input assembly files, with `None` being stdin
+    ///
+    /// Required
+    pub input: Option<(String, PathBuf)>,
+    /// How many bytes of header to skip
+    ///
+    /// Default: 0
+    pub skip: usize,
+    /// Set of `(bytes, start)` pairs describing how many `bytes` not to disassemble from position `start`
+    ///
+    /// Default: empty
+    pub keep: Vec<(usize, usize)>,
+}
+
 
 impl AssemblerOptions {
     /// Parse `env`-wide command-line arguments into an `AssemblerOptions` instance
@@ -44,7 +66,7 @@ impl AssemblerOptions {
             .about("Assembler for the pir-8")
             .setting(AppSettings::ColoredHelp)
             .args(&[Arg::from_usage("-o [BINFILE] 'Name of the the binary-file output'").default_value("a.p8b").validator(output_file_validator),
-                    Arg::from_usage("[ASMFILE]... 'Packages to update'")
+                    Arg::from_usage("[ASMFILE]... 'Files to assemble'")
                         .empty_values(false)
                         .validator(|s| if s == "-" {
                             Ok(())
@@ -67,6 +89,40 @@ impl AssemblerOptions {
                 "-" => None,
                 f => Some(output_file_process(f)),
             },
+        }
+    }
+}
+
+impl DisassemblerOptions {
+    /// Parse `env`-wide command-line arguments into an `DisassemblerOptions` instance
+    pub fn parse() -> DisassemblerOptions {
+        let matches = App::new("pir-8-disasm")
+            .version(crate_version!())
+            .author(crate_authors!())
+            .about("Disassembler for the pir-8")
+            .setting(AppSettings::ColoredHelp)
+            .args(&[Arg::from_usage("-e [BYTES] 'Skip <BYTES> bytes of header'")
+                        .default_value("0")
+                        .hide_default_value(true)
+                        .validator(|s| parse_with_prefix::<usize>(&s).map(|_| ()).ok_or_else(|| format!("\"{}\" not a number with optional base prefix", s))),
+                    Arg::from_usage("-k <START,BYTES>... 'Don't disassemble <BYTES> bytes from position <START>'")
+                        .use_delimiter(false)
+                        .number_of_values(1)
+                        .validator(|s| parse_keep(&s).map(|_| ())),
+                    Arg::from_usage("<FILE> 'Binary to disassemble'").empty_values(false).validator(|s| if s == "-" {
+                        Ok(())
+                    } else {
+                        filesystem_validator("Binary file", false, &s)
+                    })])
+            .get_matches();
+
+        DisassemblerOptions {
+            input: match matches.value_of("FILE").unwrap() {
+                "-" => None,
+                f => Some((f.to_string(), fs::canonicalize(f).unwrap())),
+            },
+            skip: parse_with_prefix(matches.value_of("e").unwrap()).unwrap(),
+            keep: matches.values_of("k").map(|kk| kk.flat_map(parse_keep).collect()).unwrap_or(vec![]),
         }
     }
 }
@@ -97,6 +153,17 @@ fn output_file_validator(s: String) -> Result<(), String> {
         } else {
             buf.canonicalize().map(|_| ()).map_err(|e| format!("Output file: {}", e))
         }
+    }
+}
+
+
+fn parse_keep(s: &str) -> Result<(usize, usize), String> {
+    match s.find(',').map(|comma_pos| s.split_at(comma_pos)).map(|(first, second)| (first, &second[1..])) {
+        Some((first, second)) => {
+            Ok((parse_with_prefix(first).ok_or_else(|| format!("\"{}\" is not a number", first))?,
+                parse_with_prefix(second).ok_or_else(|| format!("\"{}\" is not a number", second))?))
+        }
+        None => Err(format!("\"{}\" is not two numbers separated by a comma", s)),
     }
 }
 
