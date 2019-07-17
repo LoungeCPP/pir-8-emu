bc1eb695b3042a7861080be797afb09b08bc465e
nabijaczleweli <nabijaczleweli@gmail.com>
2019-07-17 02:37:45 +0200

    "Use Debug for argument-free μOps"

--- src/microcode/display.rs
+++ src/microcode/display.rs
@@ -17,30 +17,16 @@ pub struct DisplayMicroOp<'a> {
 impl<'a> fmt::Display for DisplayMicroOp<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.op {
-            MicroOp::Nop => f.write_str("Nop"),
-            MicroOp::Halt => f.write_str("Halt"),
-
-            MicroOp::StackPush => f.write_str("StackPush"),
-            MicroOp::StackPop => f.write_str("StackPop"),
-
             MicroOp::Alu(op) => write!(f, "Alu {}", op),
 
-            MicroOp::PortIn => f.write_str("PortIn"),
-            MicroOp::PortOut => f.write_str("PortOut"),
-
-            MicroOp::Compare => f.write_str("Compare"),
-
             MicroOp::MakeImmediate(imm) => write!(f, "MakeImmediate {:#04x}", imm),
-            MicroOp::LoadImmediate => f.write_str("LoadImmediate"),
-
-            MicroOp::FetchAddress => f.write_str("FetchAddress"),
-            MicroOp::WriteAddress => f.write_str("WriteAddress"),
 
             MicroOp::CheckJumpCondition(cond) => write!(f, "CheckJumpCondition {}", cond),
-            MicroOp::Jump => f.write_str("Jump"),
 
             MicroOp::ReadRegister(aaa) => write!(f, "ReadRegister {}", self.registers[*aaa as usize].letter()),
             MicroOp::WriteRegister(aaa) => write!(f, "WriteRegister {}", self.registers[*aaa as usize].letter()),
+
+            op => (op as &fmt::Debug).fmt(f),
         }
     }
 }
